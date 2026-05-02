f05c14f796ab90a371ccfa6cb67681c3100b6987
KISHAN KUMAR <kk1085035@gmail.com>
2026-05-02 23:07:19 +0530

    Time: 45 ms (62.05%), Space: 91.2 MB (13.45%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if(nums.length < 2){
+            return 0 ;
+        }
+       Arrays.sort(nums);
+       int max = nums[1] - nums[0] ;
+       for(int i = 2 ; i < nums.length ;i++){
+           max= Math.max(max , nums[i] - nums[i-1]);
+       }
+        return max ;
+    }
+}
